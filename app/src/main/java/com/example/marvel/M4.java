5f0ec8ad9b4c6728a272c22c8abe1a02ad0bc104
omarhisham55 <76630665+omarhisham55@users.noreply.github.com>
2022-04-27 05:01:21 +0200

    Add files via upload

--- app/src/main/java/com/example/marvel/M4.java
+++ app/src/main/java/com/example/marvel/M4.java
@@ -1,49 +1,57 @@
 package com.example.marvel;
 
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-//3aizen design ll profile picture el (Unknown) 3alshan tet7at gamb el listner fy M4 page 3alshan yn2a dah esm el account bta3o
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class M4 extends AppCompatActivity {
-    private Button button;
-    private Button button2;
+    public Button button;
+    RecyclerView movie;
+    List<scroll> scrollList;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_m4);
-        SharedPreferences preferences=getSharedPreferences("MyPrefs",MODE_PRIVATE);
-        String display = preferences.getString("display","");
-        button =(Button)findViewById(R.id.PlayBtn);
-        button.setOnClickListener(new View.OnClickListener() {
+
+        button = findViewById(R.id.buttonx);
+        button.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
+            public void onClick(View view){
                 open_M5();
             }
         });
-        button2 =(Button)findViewById(R.id.textView9);
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                open_M8();
-            }
-        });
         Intent intent = getIntent();
-        String message =intent.getStringExtra(M3.EXTRA_MESSAGE);
+        String message = intent.getStringExtra(M3.EXTRA_MESSAGE);
 
-        TextView textView = findViewById(R.id.text_message);
-        textView.setText(message);
+
+
+        movie = findViewById(R.id.movies);
+        scrollList = new ArrayList<>();
+
+        scrollList.add(new scroll(R.drawable.movie1));
+        scrollList.add(new scroll(R.drawable.movie2));
+        scrollList.add(new scroll(R.drawable.movie3));
+        scrollList.add(new scroll(R.drawable.movie4));
+
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        manager.setOrientation(LinearLayoutManager.HORIZONTAL);
+        movie.setLayoutManager(manager);
+
+        adaptor adaptor = new adaptor(this, scrollList);
+        movie.setAdapter(adaptor);
+
     }
-    public void open_M5(){
-        Intent intent=new Intent(M4.this,M5.class);
+    private void open_M5() {
+        Intent intent = new Intent(this, M5.class);
         startActivity(intent);
     }
-    public void open_M8(){
-        Intent intent=new Intent(M4.this,M8.class);
-        startActivity(intent);
-    }
-
 }
