520fcd3ef4148226d4f448d7f5293e9f3f22238d
rabehmohamed <81047812+rabehmohamed@users.noreply.github.com>
2022-04-27 04:47:03 +0200

    Add files via upload

---
+++ app/src/main/java/com/example/marvel/scroll.java
@@ -0,0 +1,15 @@
+package com.example.marvel;
+
+public class scroll {
+    int image;
+    int size;
+
+    public scroll(int image){
+        this.image=image;
+    }
+
+    public int getImage(){
+        return image;
+    }
+
+}
